9b13d5c3fa760b5643033a28c3d05cecf4592005
Bryce S <bnicerrr@gmail.com>
2021-05-03 15:04:57 -0400

    fix nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,58 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      disable-resize-watcher
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            <strong>MMA</strong>Fantasy
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <em
+              >"Everyone has a plan until they <br />
+              get punched in the mouth."</em
+            >
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item link to="/fighters">
+          <v-list-item-content>
+            <v-list-item-title>Fighters</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/standings">
+          <v-list-item-content>
+            <v-list-item-title>Standings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/teams">
+          <v-list-item-content>
+            <v-list-item-title>Team & Rosters</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app color="primary" dark>
-      <v-btn text to="/"><strong>MMA</strong>FANTASY</v-btn>
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        class="d-sm-none"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title><strong>MMA</strong>FANTASY</v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <v-btn text to="/fighters">Fighters</v-btn>
-      <v-btn text to="/standings">Standings</v-btn>
-      <v-btn text to="/teams">Teams & Rosters</v-btn>
+      <div class="d-none d-sm-block">
+        <v-btn text rounded to="/fighters">Fighters</v-btn>
+        <v-btn text rounded to="/standings">Standings</v-btn>
+        <v-btn text rounded to="/teams">Teams & Rosters</v-btn>
+      </div>
     </v-app-bar>
 
     <v-main>
@@ -18,9 +64,8 @@
 <script>
 export default {
   name: "App",
-
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
