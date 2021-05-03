da91b38f374ac6d8d28c90387bc382279b292874
Bryce S <bnicerrr@gmail.com>
2021-05-03 15:11:15 -0400

    remove duplicate

--- src/views/Standings.vue
+++ src/views/Standings.vue
@@ -24,20 +24,6 @@
             <v-container>
               <v-row>
                 <v-col cols="8">
-                  <span style="font-size: 1.5em">Joey Bigsby</span>
-                </v-col>
-                <v-col cols="4" class="text-right">
-                  <span style="font-size: 1.5em">0</span>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card>
-            <v-container>
-              <v-row>
-                <v-col cols="8">
                   <span style="font-size: 1.5em">Scott Caliway</span>
                 </v-col>
                 <v-col cols="4" class="text-right">
